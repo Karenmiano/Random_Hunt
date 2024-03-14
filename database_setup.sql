381b7ce301eb650c5e006df2c15be1988878b931
Karenmiano <mianokaren1798@gmail.com>
2024-03-14 17:39:44 +0300

    Removing ignored files from repo

--- database_setup.sql
+++
@@ -1,7 +0,0 @@
--- creates the database and a user for the project
-
-CREATE DATABASE IF NOT EXISTS finding_mimo_db;
-CREATE USER IF NOT EXISTS 'mimo'@'localhost' IDENTIFIED BY 'finding_mimo_2024';
-GRANT ALL PRIVILEGES ON `finding_mimo_db`.* TO 'mimo'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'mimo'@'localhost';
-FLUSH PRIVILEGES;
